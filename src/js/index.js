43581a241bf7d9f9f4d397f9a7159f8c3eb8b6b1
SamanAtashi <milady00126@gmail.com>
2021-08-10 13:23:18 +0300

    Update HTML/CSS for displaying items + basic fetch function

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
 import '../sass/style.scss';
+
+// todo:-----------> Get data from API
+
+const baseUrl = 'https://api.tvmaze.com/shows/';
+
+async function makeShowUrl(id) {
+  const temp = fetch(`${baseUrl}${id}`);
+  const temp1 = await temp;
+  const temp2 = temp1.json();
+  return temp2;
+}
