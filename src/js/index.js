53aa01b50e5273de2108e5e0d8a6b5c2f8e0b34e
SamanAtashi <milady00126@gmail.com>
2021-08-10 13:24:40 +0300

    Add function to load and put url inside each element

--- src/js/index.js
+++ src/js/index.js
@@ -41,3 +41,24 @@ function makeElementsForShow() {
   createLi.appendChild(createBtn);
   document.querySelector('#list').appendChild(createLi);
 }
+
+async function putShowInside(show, num) {
+  // bring them in
+  const temp = await show;
+  const tempImg = temp.image.original;
+  const tempName = temp.name;
+
+  // put it inside
+  const imgElements = document.querySelectorAll('.img');
+  const titleElements = document.querySelectorAll('.name');
+  imgElements[num - 1].setAttribute('src', tempImg);
+  titleElements[num - 1].innerHTML = tempName;
+}
+
+window.addEventListener('load', () => {
+  for (let i = 1; i < 7; i += 1) {
+    const temp = makeShowUrl(i);
+    makeElementsForShow();
+    putShowInside(temp, i);
+  }
+});
