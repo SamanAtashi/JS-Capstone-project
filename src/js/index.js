31f2a223bfc99fc801a16bf43219808cb7b2e96e
Caleb Anderson <buyananderson@gmail.com>
2021-08-10 16:26:07 +0200

    Make changes required

--- src/js/index.js
+++ src/js/index.js
@@ -13,29 +13,25 @@ const makeShowUrl = async (id) => {
 
 // todo:-----------> Show them on HTML (name + image)
 
-const makeElementsForShow = () => {
-  document.querySelector(
-    '#list',
-  ).innerHTML += '<li class="item"><img class="img"><div class="title d-flex center"><h2 class="name"></h2><a class="linkIcon"><i class="far fa-heart"></i></a></div><input type="button" value="Comments"></li>';
-};
-
-const putShowInside = async (show, num) => {
-  // bring them in
-  const temp = await show;
-  const tempImg = temp.image.original;
-  const tempName = temp.name;
-
-  // put it inside
-  const imgElements = document.querySelectorAll('.img');
-  const titleElements = document.querySelectorAll('.name');
-  imgElements[num - 1].setAttribute('src', tempImg);
-  titleElements[num - 1].innerHTML = tempName;
+const makeElementsForShow = async (Promisedata) => {
+  const data = await Promisedata;
+  const list = document.querySelector('#list');
+  const li = document.createElement('li');
+  li.className = 'item';
+  li.innerHTML = `
+  <img class="img" src="${data.image.original}" />
+  <div class="title d-flex center">
+    <h2 class="name">${data.name}</h2>
+    <a class="linkIcon"><i class="far fa-heart"></i></a>
+  </div>
+  <input type="button" value="Comments" />`;
+  list.appendChild(li);
 };
 
 window.addEventListener('load', () => {
   for (let i = 1; i < 7; i += 1) {
     const temp = makeShowUrl(i);
-    makeElementsForShow();
-    putShowInside(temp, i);
+    makeElementsForShow(temp);
+    // putShowInside(temp, i);
   }
 });
