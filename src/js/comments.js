b2c12d3327577026ee6820de7ad2ab3a7660911d
Caleb Anderson <buyananderson@gmail.com>
2021-08-11 13:55:32 +0200

    Export the popUp function

--- src/js/comments.js
+++ src/js/comments.js
@@ -1,40 +1,40 @@
-const sendComment = async (newData) => {
-  const response = await fetch(
-    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/0tY3ZECsTgObPiElFJSy/comments',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(newData),
-    }
-  );
-  return response.text();
-};
+// const sendComment = async (newData) => {
+//   const response = await fetch(
+//     'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/0tY3ZECsTgObPiElFJSy/comments',
+//     {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(newData),
+//     }
+//   );
+//   return response.text();
+// };
 
-const submitComment = (id, form) => {
-  form.addEventListener('submit', (e) => {
-    const inputName = document.querySelector('.input-name');
-    const inputComment = document.querySelector('.input-comment');
-    const newComment = {
-      item_id: id,
-      username: inputName.value,
-      comment: inputComment.value,
-    };
-    e.preventDefault();
-    sendComment(newComment);
-  });
-};
+// const submitComment = (id, form) => {
+//   form.addEventListener('submit', (e) => {
+//     const inputName = document.querySelector('.input-name');
+//     const inputComment = document.querySelector('.input-comment');
+//     const newComment = {
+//       item_id: id,
+//       username: inputName.value,
+//       comment: inputComment.value,
+//     };
+//     e.preventDefault();
+//     sendComment(newComment);
+//   });
+// };
 
-const createForm = (list) => {
-  const form = document.createElement('form');
-  form.innerHTML = `
-`;
-  list.appendChild(form);
-  //   submitComment(list.id, form);
-};
+// const createForm = (list) => {
+//   const form = document.createElement('form');
+//   form.innerHTML = `
+// `;
+//   list.appendChild(form);
+//   //   submitComment(list.id, form);
+// };
 
-export const popUp = (list) => {
+const popUp = (list) => {
   const popDiv = document.createElement('section');
   popDiv.className = 'bg-dark popUp';
   const img = list.querySelector('.img');
@@ -47,7 +47,7 @@ export const popUp = (list) => {
          <form >
              <input type="text" class="form-control input-name" placeholder="Your Name">
             <input type="text" class="form-control input-comment" placeholder=" Your Comment">
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">Comment</button>
          </form>
         </div>
     </div> `;
@@ -60,4 +60,4 @@ export const popUp = (list) => {
   });
 };
 
-export default createForm;
+export default popUp;
