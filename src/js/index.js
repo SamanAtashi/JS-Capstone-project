9dbb6d5854dcef59a23896904e37cea2638db3b6
SamanAtashi <milady00126@gmail.com>
2021-08-11 12:15:57 +0300

    Break makeElementForShow into 2 parts + Make empty likes.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import '../sass/style.scss';
+import { addLike } from './likes';
 
 // todo:-----------> Get data from API
 
@@ -13,25 +14,29 @@ const makeShowUrl = async (id) => {
 
 // todo:-----------> Show them on HTML (name + image)
 
-const makeElementsForShow = async (Promisedata) => {
-  const data = await Promisedata;
-  const list = document.querySelector('#list');
-  const li = document.createElement('li');
-  li.className = 'item d-flex d-col center';
-  li.innerHTML = `
-  <img class="img" src="${data.image.original}" />
-  <div class="title d-flex center">
-    <h2 class="name">${data.name}</h2>
-    <a class="linkIcon"><i class="far fa-heart"></i></a>
-  </div>
-  <input type="button" value="Comments" />`;
-  list.appendChild(li);
+const makeElementsForShow = () => {
+  document.querySelector('#list').innerHTML
+    += '<li class="item"><img class="img"><div class="title d-flex center"><h2 class="name"></h2><a class="likes"><i class="far fa-heart"></i></a></div><input type="button" value="Comments"></li>';
+};
+
+const putShowInside = async (show, num) => {
+  // bring them in
+  const temp = await show;
+  const tempImg = temp.image.original;
+  const tempName = temp.name;
+
+  // put it inside
+  const imgElements = document.querySelectorAll('.img');
+  const titleElements = document.querySelectorAll('.name');
+  imgElements[num - 1].setAttribute('src', tempImg);
+  titleElements[num - 1].innerHTML = tempName;
 };
 
 window.addEventListener('load', () => {
   for (let i = 1; i < 7; i += 1) {
     const temp = makeShowUrl(i);
-    makeElementsForShow(temp);
-    // putShowInside(temp, i);
+    makeElementsForShow();
+    putShowInside(temp, i);
   }
+  addLike(document.querySelectorAll('.likes'));
 });
