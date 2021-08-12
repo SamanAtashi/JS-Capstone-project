b2b2075b8bf93106cd91aa28c7d54f3c53ec5b2f
SamanAtashi <milady00126@gmail.com>
2021-08-12 10:07:35 +0300

    Remove comments

--- jest.config.js
+++ jest.config.js
@@ -4,22 +4,8 @@
  */
 
 module.exports = {
-  // All imported modules in your tests should be mocked automatically
-  // automock: false,
-
-  // Stop running tests after `n` failures
-  // bail: 0,
-
-  // The directory where Jest should store its cached dependency information
-  // cacheDirectory: "/private/var/folders/tc/7cxddhf144gdn1dy2jfwnw5w0000gp/T/jest_dy",
-
-  // Automatically clear mock calls and instances between every test
   clearMocks: true,
-
-  // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
-
   coverageDirectory: 'coverage',
-
   testEnvironment: 'jsdom',
 };
